6128b71601f22dcfce08a3f9d1b78f9b84f2bc9b
genius158 <yanxianwei@didiglobal.com>
2021-12-24 15:46:30 +0800

    feat: feat: mojo msg send recvd

--- mojotest/demo_mojo_multiple_process.cc
+++ mojotest/demo_mojo_multiple_process.cc
@@ -39,33 +39,60 @@ using namespace demo::mojotest::mojom;
 
 #pragma region Test
 class TestImpl : public Test {
-// public:
-//  void Hello(const std::string& caller) override {
-//    caller_ = caller;
-//    LOG(INFO) << "Test1 run: Hello " << caller;
-//  }
-//  void Hi(HiCallback callback) override {
-//    LOG(INFO) << "Test1 run: Hi " << caller_;
-//    std::move(callback).Run(caller_);
-//  }
-// private:
-//  std::string caller_;
+private:
+    std::string caller_;
+
+public:
+    explicit TestImpl(mojo::PendingReceiver<Test> receiver):receiver_(this,std::move(receiver)){}
+
+    void Hello(const std::string& caller) override {
+        caller_ = caller;
+        LOG(INFO) << "TestImpl run: Hello From " << caller << " " << getpid();;
+    }
+    void Hi(HiCallback callback) override {
+        std::string txt = "TestImpl run: TestImpl Hi To ";
+
+        LOG(INFO) << txt << caller_;
+//        std::move(callback).Run(txt.append(caller_));
+        std::move(callback).Run(txt);
+        LOG(INFO) << "std::move(callback).Run(txt); " << getpid();;
+
+    }
 
 /* ------------------------------------------- */
-    private:
+
+private:
     mojo::Receiver<Test> receiver_;
 
-    public:
-        explicit TestImpl(mojo::PendingReceiver<Test> receiver):receiver_(this,std::move(receiver)){}
+public:
+    void SendHandle(mojo::ScopedMessagePipeHandle pipe_handle) override {
+        LOG(INFO) <<"SendHandle: " << pipe_handle->value()<< "  "<<getpid();
 
-        void SendHandle(mojo::ScopedMessagePipeHandle pipe_handle) override {
-            LOG(INFO) <<"SendHandle: " << pipe_handle->value();
-        }
+        //if(!pipe->QuerySignalsState().readable()){
+            // msg form client
+            MojoResult result;
+            result = mojo::Wait(pipe_handle.get(),MOJO_HANDLE_SIGNAL_READABLE);
+            std::vector<uint8_t> data;
+            result = mojo::ReadMessageRaw(pipe_handle.get(),&data,nullptr,MOJO_READ_MESSAGE_FLAG_NONE);
+            LOG(INFO) << "MSG "<< (char*)&data[0];
 
 
+            // to client
+            const char KMessage[] = "HI";
+            mojo::WriteMessageRaw(pipe_handle.get(), KMessage,sizeof(KMessage),nullptr,0,MOJO_WRITE_MESSAGE_FLAG_NONE);
+
+
+      //  }
+        LOG(INFO) <<"SendHandle End: " << pipe_handle->value()<< "  "<<getpid();
+
+    }
 };
 #pragma endregion
 
+
+void OnBack(const std::string& msg){
+    LOG(INFO) << "test response: Hi " << msg << "---" << getpid();
+}
 
 void MojoProducer(){
     mojo::PlatformChannel channel;
@@ -74,7 +101,7 @@ void MojoProducer(){
     mojo::OutgoingInvitation invitation;
 
     mojo::ScopedMessagePipeHandle pipe = invitation.AttachMessagePipe("test");
-    LOG(INFO) << "pipe value : " << pipe->value();
+    LOG(INFO) << "MojoProducer pipe value : " << pipe->value() << "  " <<getpid();
 
     base::LaunchOptions options;
     base::CommandLine command_line(base::CommandLine::ForCurrentProcess()->GetProgram());
@@ -87,19 +114,37 @@ void MojoProducer(){
 
     mojo::InterfacePtr<Test> test;
     test.Bind(mojo::InterfacePtrInfo<Test>(std::move(pipe), 0));
-    test->SendHandle(std::move(pipe));
-    LOG(INFO) << "Test call: SendHandle";
+    LOG(INFO) << "Test call: SendHandle  " << getpid();
+    test->Hello("MojoProducer");
 
+    test->Hi(base::BindOnce(&OnBack));
+
+    mojo::MessagePipe msgPipe;
+    LOG(INFO) << "pipe0: "<<msgPipe.handle0->value() <<" pipe1: "<<msgPipe.handle1->value()<<"  " << getpid();
+    test->SendHandle(std::move(msgPipe.handle1));
+
+    // send msg to remote
+    const char KMessage[] = "Hello";
+    mojo::WriteMessageRaw(msgPipe.handle0.get(), KMessage,sizeof(KMessage),nullptr,0,MOJO_WRITE_MESSAGE_FLAG_NONE);
+
+    // get msg from remote
+    MojoResult result;
+    result = mojo::Wait(msgPipe.handle0.get(),MOJO_HANDLE_SIGNAL_READABLE);
+    std::vector<uint8_t> data;
+    result = mojo::ReadMessageRaw(msgPipe.handle0.get(),&data,nullptr,MOJO_READ_MESSAGE_FLAG_NONE);
+    LOG(INFO) << "MSG handle0 "<< (char*)&data[0];
+
+    LOG(INFO) << "test->Hi(base::BindOnce([](const std::string& msg) end" << getpid();
 }
+
 
 void MojoConsumer(){
     mojo::IncomingInvitation invitation = mojo::IncomingInvitation::Accept(mojo::PlatformChannel::RecoverPassedEndpointFromCommandLine(*base::CommandLine::ForCurrentProcess()));
 
     mojo::ScopedMessagePipeHandle pipe = invitation.ExtractMessagePipe("test");
-    LOG(INFO) << "pipe value : " << pipe->value();
+    LOG(INFO) << "MojoConsumer pipe value : " << pipe->value()<< " " << getpid();
 
     new TestImpl(mojo::PendingReceiver<Test>(std::move(pipe)));
-
 }
 
 int main(int argc,char** argv){
@@ -119,7 +164,7 @@ int main(int argc,char** argv){
         MojoConsumer();
     }
 
-    LOG(INFO) << "running...";
+    LOG(INFO) << "running... "<< getpid();
     run_loop.Run();
     return 0;
 
